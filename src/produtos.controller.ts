5b0bc74586c46f61bb628568ebc4a75e9a4535aa
Vinicius Abreu Caldeira <viniciusabreu00@outlook.com>
2021-09-16 17:54:25 -0300

    Criando o CRUD

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,11 +1,36 @@
-import { Controller, Get } from "@nestjs/common";
-
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Produto } from "./produto.model";
 
 @Controller('produtos')
 export class ProdutosController {
+    produtos: Produto[] = [
+        new Produto("LIV001", "Livro de TDD e BDD na prática", 29.5),
+        new Produto("LIV002", "Livro BRIMBA", 45.3),
+        new Produto("LIV003", "Livro Brerbss", 25.5)
+    ]
     
     @Get()
-    obterTodos() : string {
-        return  'Lista todos os produtos'
+    obterTodos() : Produto[]{
+        return  this.produtos
+    }
+    @Get(':id')
+    obterUm(@Param() params): Produto {
+        return this.produtos[0]
+    }
+
+    @Post()
+    criar(@Body() produto: Produto){
+        produto.id = 100
+        this.produtos.push(produto)
+    }
+
+    @Put()
+    alterar(@Body() produto): Produto{
+        return produto;
+    }
+
+    @Delete(':id')
+    apagar(@Param() params){
+        this.produtos.pop();
     }
 }
